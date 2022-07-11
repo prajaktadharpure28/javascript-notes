83b066e02ecd39f1720c746e942be51cb1f8081a
prajaktadharpure28 <prajaktadharpure28@gmail.com>
2022-07-11 15:57:06 +0530

    Added context.js

---
+++ Basic/12context.js
@@ -0,0 +1,36 @@
+tipper("80");
+function tipper(a){
+    var bill = parseInt(a);
+    console.log(bill + 5);
+}
+
+// function bigTipper(a){
+//     var bill = parseInt(a);
+//     console.log(bill + 15);
+// }
+
+// bigTipper("200");
+
+
+
+var bigTipper = function (a){
+    var bill = parseInt(a);
+    console.log(bill + 15);
+};
+bigTipper("200");
+
+
+console.log(name);
+var name ="praju";
+
+
+var name ="praju";
+console.log(name);
+
+function sayName(){
+    var name1 = "hello";
+    console.log(name1);
+}
+sayName();
+
+console.log(name);
