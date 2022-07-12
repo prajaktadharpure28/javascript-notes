ae5e71b34b6a5a4507f7ad7496c5882903ec2677
prajaktadharpure28 <prajaktadharpure28@gmail.com>
2022-07-12 16:40:24 +0530

    Added template literal

---
+++ Basic/44template-literal.js
@@ -0,0 +1,8 @@
+var name = `praju`
+var fullName = `${name} dharpure`;
+
+console.log(fullName);
+console.log(fullName.charAt(0));
+console.log(fullName.endsWith("?"));
+console.log(fullName.includes("praju"));
+console.log(fullName.toUpperCase());
